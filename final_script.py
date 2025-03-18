a94ef92223d88bc6a06cbd17a811e53337604310
omarstinner-careem <150437952+omarstinner-careem@users.noreply.github.com>
2025-03-19 01:48:48 +0400

    Update final_script.py

--- final_script.py
+++ final_script.py
@@ -635,7 +635,7 @@ fig1.add_annotation(
 
 #Year Split Title
 fig1.add_annotation(
-    x=10,
+    x=5,
     y=15,
     text="<b>                        Yearly Experiment Split                        </b>",
     showarrow=False,
